c7fc1b263f6d7808a55f584e67fbf5c405cab0d8
DrippinMf <106818811+DrippinMf@users.noreply.github.com>
2022-12-02 20:49:43 +0700

    Add files via upload

---
+++ Endgame Project/scriptmain.js
@@ -0,0 +1,10 @@
+var ms = [
+	{id , grade: "oneto44" },
+	{name: Ironman , franchise: Marvel},
+	{name: Thor , franchise: Marvel},
+	{name: Superman , franchise: DC}
+];
+
+var hg =  ms.filter(function(m) {
+	return ms.grade == "oneto44";
+});
